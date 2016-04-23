c61003dcb764d234e464215c021906c5799a5a5f
Robert Lutz <robertlutzi@gmail.com>
2016-04-22 21:12:50 -0400

    feat: Introduce a Q-Learning Agent

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -32,68 +32,85 @@ class StochasticAgent(Agent):
 
 #==============================================================================
 
-class DimAwarenessAgent(Agent):
+class DimAwarenessAgent(StochasticAgent):
     """
     An agent is aware of the state its in, but is not able to reason and plan
-    based upon this knowledge.
+    based upon this knowledge. Satisfies the second part of the assignment.
     """
 
     def __init__(self, env):
         super(DimAwarenessAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
-        self.color = 'black'  # override color
-        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
-
-    def reset(self, destination=None):
-        self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
 
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-        self.state = [inputs['light'], inputs['oncoming'], inputs['left'], self.next_waypoint, deadline]
+        self.state = (inputs['light'], inputs['oncoming'], inputs['left'], self.next_waypoint)
         action = self.env.valid_actions[random.randrange(4)]
         # Execute action and get reward
         reward = self.env.act(self, action)
-
-        # TODO: Learn policy based on state, action, reward
-
         print "LearningAgent.update():deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
-class LearningAgent(Agent):
+#==============================================================================
+
+class QLearningAgent(StochasticAgent):
     """An agent that learns to drive in the smartcab world."""
 
     def __init__(self, env):
-        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
-        self.color = 'black'  # override color
-        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
+        super(QLearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        self.states = [ (light, oncoming, left, waypt)
+                for light in ('red', 'green')
+                for oncoming in ('left', 'right', 'forward', None)
+                for left in ('left', 'right', 'forward', None)
+                for waypt in ('left', 'right', 'forward') ]
+        self.Q = { (state, action) : 0
+                for state in self.states
+                for action in self.env.valid_actions }
+        self.policy = { state : 'left'
+                for state in self.states }
+        self.gamma = 0.9
+        self.alpha = 0.25
+        self.policy_needs_updated = False
 
-    def reset(self, destination=None):
-        self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
+    def __update_Q__(self):
+        Q_prime = max([self.Q[self.state, ax]
+                        for ax in ('left', 'right', 'forward', None)])
+        self.Q[self.previous_state, self.action] = (
+                    (1 - self.alpha) * self.Q[self.previous_state, self.action]
+                    + self.alpha * (self.reward + self.gamma * Q_prime)
+                    )
+
+    def __update_policy__(self):
+        #self.alpha = 1/(1./self.alpha + 0.1)
+        self.__update_Q__()
+        self.policy[self.previous_state] = max(
+                                [(ax, self.Q[self.previous_state, ax])
+                                for ax in ('left', 'right', 'forward', None)],
+                                key= lambda x: x[1]
+                                )[0]
+
+    def __choose_action__(self):
+        return self.policy[self.state]
 
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-
-        # TODO: Update state
-
-        # TODO: Select action according to your policy
-        x = random.randrange(4)
-        #action = (None, 'forward', 'right', 'left')[x]
-        action = self.next_waypoint
+        self.state = (inputs['light'], inputs['oncoming'], inputs['left'],
+                        self.next_waypoint)
+        if self.policy_needs_updated:
+            self.__update_policy__()
+        self.action = self.__choose_action__()
         # Execute action and get reward
-        reward = self.env.act(self, action)
+        self.reward = self.env.act(self, self.action)
+        # Learn policy based on state, action, reward
+        self.previous_state = self.state
+        self.policy_needs_updated = True
+        print "LearningAgent.update():deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, self.action, self.reward)  # [debug]
 
-        # TODO: Learn policy based on state, action, reward
-
-        print "LearningAgent.update():deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-
+#==============================================================================
 
 def run(agent, enforce, delay):
     """Run the agent for a finite number of trials."""
@@ -109,5 +126,6 @@ def run(agent, enforce, delay):
 
 
 if __name__ == '__main__':
-    run(StochasticAgent, enforce=False, delay=0.2)
+    run(StochasticAgent, enforce=False, delay=1)
     run(DimAwarenessAgent, enforce=False, delay=1)
+    run(QLearningAgent, enforce=True, delay=1)
